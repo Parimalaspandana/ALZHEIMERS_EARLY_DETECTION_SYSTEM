6ea4d29cd9db977afafaca0cb28ff014e5ee760f
yesh <parimalaspandana@gmail.com>
2026-02-06 16:43:17 +0530

    Initial committeddd

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
-from .inference import predict_image 
+from inference import predict_image 
+# OR, if 'backend' is treated as a package:
+from backend.inference import predict_image
 
 app = FastAPI(title="Alzheimer MRI Image Prediction API")
 
